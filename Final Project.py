3172aff0b7fdb6948823fea8349b12954c5e2f57
Mbeaver003 <43656010+Mbeaver003@users.noreply.github.com>
2019-03-11 13:02:19 -0400

    Update Final Project.py

--- Final Project.py
+++ Final Project.py
@@ -1,5 +1,11 @@
 # Matthew Beaver
 # Final Exam
 
+visitor_count = 0
+possible_visitor_count = 0
 
-print("I did it")
+Q1 = def(input("Would you like to have a website built for you? Y/N")):
+
+if Q1 = Y:
+    then print("We can do that for you!")
+    else print("Maybe another time.")
