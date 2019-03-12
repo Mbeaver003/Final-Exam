53fbf19a4e1d2b686ae518c655dec364e8ff7f79
Matthew Beaver <02matthew.beaver@gulllakecs.org>
2019-03-12 11:58:50 -0400

    Final Exam 2

--- Final Project.py
+++ Final Project.py
@@ -1,5 +1,14 @@
 # Matthew Beaver
 # Final Exam
 
+#In this project, I am going to create a program that asks people whether or not they would want my website building buisness
 
-print("I did it")
+
+visitor_count = 0
+possible_visitor_count = 0
+
+Q1 = def(input("Would you like to have a website built for you? Y/N")):
+
+if Q1 = Y:
+    then print("We can do that for you!")
+    else print("Maybe another time.")
